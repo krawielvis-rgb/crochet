a01143880243bc83d4af2bc94f901e64322de932
krawielvis-rgb <krawielvis@gmail.com>
2026-09-19 10:47:15 +0100

    RESTORE publish route via sara-publisher (homepage cards on publish)

--- src/tutorial-image-wrapper.js
+++ src/tutorial-image-wrapper.js
@@ -1,4 +1,5 @@
 import baseWorker from './worker-v2.js';
+import { publishSaraHtml } from './sara-publisher.js';
 
 const REMOVED_SLUG = 'crochet-sunglasses-case';
 const GOOGLE_VERIFY = 'eYCLA4SbSc8jRmc8bI729wq-QkDGAI2F5ctE3aKDy9o';
@@ -49,6 +50,10 @@ export default {
   async fetch(request, env, ctx) {
     const url = new URL(request.url);
 
+    if (url.pathname === '/api/admin/publish-html' && request.method === 'POST') {
+      return publishSaraHtml(request, env);
+    }
+
     const slug = url.pathname.replace(/^\/posts\//, '').replace(/\.html$/, '').replace(/\/$/, '');
 
     if (slug === REMOVED_SLUG) {
@@ -79,4 +84,4 @@ export default {
   }
 };
 
-// Deployment trigger: 2026-09-19 worker fix
+// Deployment trigger: 2026-09-19 restore publish
